06e3e52b0da123fe327a2752d81d3808c8c97419
Assz <renanguides@hotmail.com>
2020-11-29 01:44:32 -0400

    Inicializando Projeto Twitter/ Inicio Backend
    Criação do projeto ate a adição de criar e listar tweets

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose')
+
+ 
+const app = express(); 
+
+mongoose.connect('mongodb+srv://Assz:coiote00@cluster-assz.yxeq8.mongodb.net/projTwitter?retryWrites=true&w=majority',{ 
+    useUnifiedTopology: true, 
+    useNewUrlParser: true 
+    }
+);
+
+app.use(express.json());
+app.use(require('./routes'));
+
+app.listen(3000,() => {
+    console.log("Servidor iniciado na porta 3000");
+});
