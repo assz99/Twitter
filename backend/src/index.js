1efc7b90a4bd59edc53a35b2406643c6b65eb57f
Assz <renanguides@hotmail.com>
2020-11-29 02:15:13 -0400

    Adição do modo IO para adição de eventos
    Adição de eventos de like e tweet

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
  
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect('mongodb+srv://Assz:coiote00@cluster-assz.yxeq8.mongodb.net/projTwitter?retryWrites=true&w=majority',{ 
     useUnifiedTopology: true, 
     useNewUrlParser: true 
     }
 );
+
+app.use((req,res, next) =>{
+    req.io = io;
+    return next();
+});
 
 app.use(express.json());
 app.use(require("./routes"));
