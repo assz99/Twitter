b38c47ef1ab3dc1912a53e42a4af94ba5aa611cc
Assz <renanguides@hotmail.com>
2020-11-30 01:26:59 -0400

    Botao Like criado e funcional

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,39 +1,57 @@
 import React, { Component } from "react";
+import api from '../services/api';
 
 import twitterLogo from '../twitter.svg';
 import './Timeline.css';
 
+import Tweet from '../components/Tweet'; 
+
 export default class Timeline extends Component {
     state = {
+        tweets:[],
         newTweet: '',
-
     };
 
-    handleNewTweet = e =>{
-        if(e.keyCode !== 13)return;
+    async componentDidMount(){
+        const response = await api.get("tweets");
+
+        this.setState({ tweets: response.data })
+    };
+
+    handleNewTweet = async e => {
+        if (e.keyCode !== 13) return;
 
         const content = this.state.newTweet;
         const author = localStorage.getItem("@GoTwitter:username");
 
-        console.log(content, author);
+        await api.post('tweets' , { content, author });
+
+        this.setState({newTweet: ''});
+
     };
 
     handleInputChange = e => {
-        this.setState({newTweet: e.target.value });
+        this.setState({ newTweet: e.target.value });
     }
 
     render() {
         return (
             <div className="timeline-wrapper">
-                <img height={24} src={twitterLogo} alt="GoTwitter"/>
+                <img height={24} src={twitterLogo} alt="GoTwitter" />
                 <form>
                     <textarea
                         value={this.state.newTweet}
-                        onChange= {this.handleInputChange}
-                        onKeyDown= {this.handleNewTweet}
+                        onChange={this.handleInputChange}
+                        onKeyDown={this.handleNewTweet}
                         placeholder="O que esta acontecendo?"
-                        ></textarea>
+                    ></textarea>
                 </form>
+                <ul className="tweet-list">
+                {this.state.tweets.map(tweet=> (
+                <Tweet key={tweet._id} tweet={tweet}>
+                </Tweet>
+                ))}
+                </ul>
             </div>
 
         );
